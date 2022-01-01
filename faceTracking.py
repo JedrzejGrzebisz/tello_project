028b50eda558fd0f4e82b20f51bb210a5d926d81
Jedrzej Grzebisz <jg.24@interia.pl>
2022-01-01 15:41:45 +0100

    add upDown tracking, fix mistakes in faceArea calc

--- faceTracking.py
+++ faceTracking.py
@@ -18,7 +18,13 @@ w = 480
 h = 320
 
 pid_yaw = [0.4, 0, 0.4]
+pid_ud = [0.4, 0, 0.4]
+pid_fb = [0.4, 0, 0.4]
 pError_yaw = 0
+pError_ud = 0
+pError_fb = 0
+
+
 def findingFace(img):
     faceCascade = cv2.CascadeClassifier("res/haarcascade_frontalface_default.xml")
     grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -30,32 +36,40 @@ def findingFace(img):
         cv2.rectangle(img, (x, y), (x+local_w, y+local_h), (0, 255, 0), 2)
         centerX = x + int(local_w/2)
         centerY = y + int(local_h/2)
-        faceArea = int(centerX * centerY)
+        faceArea = int(local_w * local_h)
         cv2.circle(img, (centerX, centerY), 5, (255, 0, 0), cv2.FILLED)
         # Lista dla wszystkich znalezionych twarzy(bo może się pojawić więcej niż jedna w kamerze)
         myFaceCenterList.append([centerX, centerY])
         myFaceAreaList.append(faceArea)
         #print("Środek twarzy:", centerX, centerY, "Pole twarzy:", faceArea)
-    if len(myFaceCenterList) != 0: # Warnek że jest co najmniej jedna twarz na kamerze
+    if len(myFaceCenterList) != 0: # Warunek że jest co najmniej jedna twarz na kamerze
         maxFaceIndex = myFaceAreaList.index(max(myFaceAreaList)) # Szukamy największej twarzy - ta będzie śledzona
         return img, [myFaceCenterList[maxFaceIndex], myFaceAreaList[maxFaceIndex]] # Zwracamy obraz kamery i dane do śledzenia
     else:
         return img, [[0, 0], 0] # Brak twarzy to zerowe parametry
 
-def trackingFace(trackingInfo, w, pid_yaw, pError_yaw):
+
+def trackingFace(trackingInfo, w, h, pid_yaw, pError_yaw, pid_ud, pError_ud, pid_fb, pError_fb):
     centerX, centerY = trackingInfo[0]
     faceArea = trackingInfo[1]
     #PID of yaw velocity
     yaw_error = centerX - int(w/2)
     yaw_vel = pid_yaw[0] * yaw_error + pid_yaw[2] * (yaw_error - pError_yaw)
     yaw_vel = int(np.clip(yaw_vel, -50, 50))
-    if centerX == 0:
+    #PID of up/down velocity
+    ud_error = int(h/2) - centerY
+    ud_vel = pid_ud[0] * ud_error + pid_ud[2] * (ud_error - pError_ud)
+    ud_vel = int(np.clip(ud_vel, -50, 50))
+    #PID of forward/backward velocity
+
+    #Sprawdzenie czy wykryto jakąkolwiek twarz
+    if centerX == 0 or centerY == 0 or faceArea == 0:
         yaw_vel = 0
+        ud_vel = 0
 
-    # myTello.send_rc_control(0, 0, 0, yaw_vel)
-    print(yaw_vel)
-
-    return yaw_error
+    # myTello.send_rc_control(0, 0, ud_vel, yaw_vel)
+    print(yaw_vel, ud_vel, faceArea)
+    return yaw_error, ud_error
 
 cap = cv2.VideoCapture(0)
 # Pętla główna programu
@@ -64,6 +78,6 @@ while True:
     success, img = cap.read()
     img = cv2.resize(img, (w, h))
     img, info = findingFace(img)
-    pError_yaw = trackingFace(info, w, pid_yaw, pError_yaw)
+    pError_yaw, pError_ud = trackingFace(info, w, h, pid_yaw, pError_yaw, pid_ud, pError_ud, pid_fb, pError_fb)
     cv2.imshow("Kamera", img)
     cv2.waitKey(1)
