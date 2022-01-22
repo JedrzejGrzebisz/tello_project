09a56806e5e73b2f4385ffa35e8eb515cffce31c
Kacper Jablonski <kapsel111@interia.eu>
2022-01-22 01:56:28 +0100

    Dodanie zapisu zdjec do folderu z obrazu z kamery

--- faceTracking.py
+++ faceTracking.py
@@ -1,6 +1,7 @@
 import cv2
 from djitellopy import tello
 import numpy as np
+import keyboard
 import time
 
 # Inicjalizacja drona
@@ -25,7 +26,8 @@ pError_ud = 0
 pError_fb = 0
 
 # Zakres braku poruszania się przód/tył
-fbRange = [4000, 8000]
+fbRange = [5000, 9000]
+
 
 def findingFace(img):
     faceCascade = cv2.CascadeClassifier("res/haarcascade_frontalface_default.xml")
@@ -35,41 +37,42 @@ def findingFace(img):
     myFaceCenterList = []
     myFaceAreaList = []
     for (x, y, local_w, local_h) in allFaces:
-        cv2.rectangle(img, (x, y), (x+local_w, y+local_h), (0, 255, 0), 2)
-        centerX = x + int(local_w/2)
-        centerY = y + int(local_h/2)
+        cv2.rectangle(img, (x, y), (x + local_w, y + local_h), (0, 255, 0), 2)
+        centerX = x + int(local_w / 2)
+        centerY = y + int(local_h / 2)
         faceArea = int(local_w * local_h)
         cv2.circle(img, (centerX, centerY), 5, (255, 0, 0), cv2.FILLED)
         # Lista dla wszystkich znalezionych twarzy(bo może się pojawić więcej niż jedna w kamerze)
         myFaceCenterList.append([centerX, centerY])
         myFaceAreaList.append(faceArea)
-        #print("Środek twarzy:", centerX, centerY, "Pole twarzy:", faceArea)
-    if len(myFaceCenterList) != 0: # Warunek że jest co najmniej jedna twarz na kamerze
-        maxFaceIndex = myFaceAreaList.index(max(myFaceAreaList)) # Szukamy największej twarzy - ta będzie śledzona
-        return img, [myFaceCenterList[maxFaceIndex], myFaceAreaList[maxFaceIndex]] # Zwracamy obraz kamery i dane do śledzenia
+        # print("Środek twarzy:", centerX, centerY, "Pole twarzy:", faceArea)
+    if len(myFaceCenterList) != 0:  # Warunek że jest co najmniej jedna twarz na kamerze
+        maxFaceIndex = myFaceAreaList.index(max(myFaceAreaList))  # Szukamy największej twarzy - ta będzie śledzona
+        return img, [myFaceCenterList[maxFaceIndex],
+                     myFaceAreaList[maxFaceIndex]]  # Zwracamy obraz kamery i dane do śledzenia
     else:
-        return img, [[0, 0], 0] # Brak twarzy to zerowe parametry
+        return img, [[0, 0], 0]  # Brak twarzy to zerowe parametry
 
 
 def trackingFace(trackingInfo, w, h, pid_yaw, pError_yaw, pid_ud, pError_ud, pid_fb, pError_fb):
     centerX, centerY = trackingInfo[0]
     faceArea = trackingInfo[1]
-    #PID of yaw velocity
-    yaw_error = centerX - int(w/2)
+    # PID of yaw velocity
+    yaw_error = centerX - int(w / 2)
     yaw_vel = pid_yaw[0] * yaw_error + pid_yaw[2] * (yaw_error - pError_yaw)
     yaw_vel = int(np.clip(yaw_vel, -50, 50))
-    #PID of up/down velocity
-    ud_error = int(h/2) - centerY
+    # PID of up/down velocity
+    ud_error = int(h / 2) - centerY
     ud_vel = pid_ud[0] * ud_error + pid_ud[2] * (ud_error - pError_ud)
     ud_vel = int(np.clip(ud_vel, -50, 50))
-    #PID of forward/backward velocity
+    # PID of forward/backward velocity
     if faceArea > fbRange[0] and faceArea < fbRange[1]:
         fb_vel = 0
     elif faceArea > fbRange[1]:
         fb_vel = -15
     elif faceArea < fbRange[0] and faceArea != 0:
         fb_vel = 15
-    #Sprawdzenie czy wykryto jakąkolwiek twarz
+    # Sprawdzenie czy wykryto jakąkolwiek twarz
     if centerX == 0 or centerY == 0 or faceArea == 0:
         yaw_vel = 0
         ud_vel = 0
@@ -79,15 +82,20 @@ def trackingFace(trackingInfo, w, h, pid_yaw, pError_yaw, pid_ud, pError_ud, pid
     print(fb_vel, yaw_vel, ud_vel, faceArea)
     return yaw_error, ud_error
 
-#cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 # Pętla główna programu
 while True:
     img = myTelloDrone.get_frame_read().frame
-    #success, img = cap.read()
+    # success, img = cap.read()
     img = cv2.resize(img, (w, h))
     img, info = findingFace(img)
     pError_yaw, pError_ud = trackingFace(info, w, h, pid_yaw, pError_yaw, pid_ud, pError_ud, pid_fb, pError_fb)
     cv2.imshow("Kamera", img)
+    #if cv2.waitKey(0.01) & 0xFF == ord('z'):
+    #    cv2.imwrite(f"zdj/{time.time()}.jpg", img)
+    if keyboard.is_pressed('z'):
+        cv2.imwrite(f"zdj/{time.time()}.jpg", img)
+        time.sleep(0.3)
     if cv2.waitKey(1) & 0xFF ==ord('q'):
         myTelloDrone.send_rc_control(0, 0, 0, 0)
         myTelloDrone.land()
